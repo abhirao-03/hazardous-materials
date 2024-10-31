4da5f1b634fc51b9f96d89ea450b166202381477
s2230095 <s2230095@ed.ac.uk>
2024-10-31 12:13:56 +0000

    consolidated jungle

--- consolidated/animate.py
+++ consolidated/animate.py
@@ -1,1 +1,25 @@
-from workspace import U_tracked
+import numpy as np
+import model_params as model
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+from workspace import *
+
+U_tracked = U
+
+min_concentration = 0
+max_concentration = 0.05
+
+fig, ax = plt.subplots()
+heatmap = ax.imshow(U_tracked[0].reshape(parameters.Nx_points, parameters.Ny_points), cmap='magma', vmin=min_concentration, vmax=max_concentration, interpolation='nearest')
+ax.set_title("Heatmap over Time")
+
+def update(frame):
+    heatmap.set_array(U_tracked[frame].reshape(parameters.Nx_points, parameters.Ny_points))
+    ax.set_title(f"Time Step: {frame}")
+    return heatmap,
+
+ani = FuncAnimation(fig, update, frames=parameters.Nt_points, interval=10, blit=False, repeat=True)
+
+plt.colorbar(heatmap, ax=ax)
+plt.show()
--- consolidated/model_params.py
+++ consolidated/model_params.py
@@ -14,7 +14,7 @@ class parameters():
         model_parameters Object
             Containing all the defined parameters
     """
-    def __init__(self, len_x=1.0, len_y=1.0, Nx_points=100, Ny_points=100, t_end=10.0, Nt_points=1000):
+    def __init__(self, len_x=1.0, len_y=1.0, Nx_points=100, Ny_points=100, t_end=1.0, Nt_points=10000):
         self.len_x = len_x
         self.len_y = len_y
         self.Nx_points = Nx_points
@@ -27,4 +27,6 @@ class parameters():
         self.dt = self.t_end / self.Nt_points
 
         self.Cx = self.dt / (self.dx ** 2)
-        self.Cy = self.dt / (self.dt ** 2)
+        self.Cy = self.dt / (self.dy ** 2)
+
+        self.diffusion_coeff = 10e-5

1209f04c294ce06f558827013ae31234dba0a501
s2230095 <s2230095@ed.ac.uk>
2024-11-07 17:51:56 +0000

    fixed time issue.

--- app/2D/workspace.py
+++ app/2D/workspace.py
@@ -3,9 +3,11 @@ import numpy as np
 import scipy.sparse as sp
 import model_params as model
 
-gas_canister = sg.GasCan2D(x_loc=0.8, y_loc=0.8, radius=0.1, concentration=1.0)
+gas_canister = sg.GasCan2D(x_loc=0.8, y_loc=0.8, radius=0.23, concentration=1.0)
+
 scrubbers = [sg.Scrubber2D(x_loc=0.5, y_loc=0.7, radius=0.08, efficiency=100.0),
                 sg.Scrubber2D(x_loc=0.5, y_loc=0.3, radius=0.08, efficiency=100.0)]
+
 scrubbers = [sg.Scrubber2D(x_loc=0.5, y_loc=0.7, radius=0.08, efficiency=0),
                 sg.Scrubber2D(x_loc=0.5, y_loc=0.3, radius=0.08, efficiency=0)]
 
